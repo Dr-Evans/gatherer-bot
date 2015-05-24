7d383223a56b409a6850992b58ce5f0e0e066b20
Nick Evans <nickevans@ufl.edu>
2015-05-24 04:10:35 -0400

    added abstract harpoon fisher class

--- src/fishing/HarpoonFisher.java
+++ src/fishing/HarpoonFisher.java
@@ -1,5 +1,31 @@
 package fishing;
 
-public class HarpoonFisher {
+import org.osbot.rs07.api.model.Entity;
+import org.osbot.rs07.script.Script;
 
+public abstract class HarpoonFisher extends Fisher {
+
+	HarpoonFisher(Script script) {
+		super(script);
+	}
+	
+	@Override
+	protected void gather() {
+		Entity fishingSpot = selectResource();
+        
+		if (fishingSpot != null) {
+			fishingSpot.interact("Harpoon");
+		}
+	}   
+	
+	@Override
+	protected void deposit() {
+		getScript().bank.depositAllExcept("Harpoon");
+	}
+	
+	@Override
+	public String toString() {
+		return "Harpoon " + super.toString();
+	}
+	
 }
